2aa94d7c09bf73bc28c103593303d8ea854a7d03
roflcoopter <jespernilsson93@hotmail.se>
2019-04-19 18:31:22 +0200

    Fix for when there is no upcoming media
    Currently the card errors out if there is no upcoming media.
    If you have for example first radarr, then sonarr and radarr is empty, sonarr will never display.
    This fixes that.

--- upcoming-media-card.js
+++ upcoming-media-card.js
@@ -19,6 +19,9 @@ class UpcomingMediaCard extends HTMLElement {
     }
     service = service ? this.config.service : this.config.entity.slice(7,11);
     const json = JSON.parse(hass.states[entity].attributes.data);
+    if (!json || !json.length) {
+      return
+    }
     const view = this.config.image_style || 'poster';
     const dateform = this.config.date || 'mmdd';
     const icon = this.config.icon || json[0]['icon'];
