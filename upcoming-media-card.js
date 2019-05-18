940ed132791e46f9c5d86ce0d95827fc7e8422ab
Ryan Meek <contact@ninjacyb.org>
2019-05-17 22:55:02 -0400

    Fix typo and lint

--- upcoming-media-card.js
+++ upcoming-media-card.js
@@ -1,16 +1,15 @@
 class UpcomingMediaCard extends HTMLElement {
-
   set hass(hass) {
     if (!this.content) {
-      const card = document.createElement('ha-card');
+      const card = document.createElement("ha-card");
       card.header = this.config.title;
-      this.content = document.createElement('div');
-      this.content.style.padding = '5px 10px';
+      this.content = document.createElement("div");
+      this.content.style.padding = "5px 10px";
       card.appendChild(this.content);
       this.appendChild(card);
     }
     // The Great Wall of Config & Defaults™
-    let style = document.createElement('style');
+    let style = document.createElement("style");
     style.setAttribute("id", "umc_style");
     var service = this.config.service;
     const entity = this.config.entity || `sensor.${service}_upcoming_media`;
@@ -18,47 +17,86 @@ class UpcomingMediaCard extends HTMLElement {
       console.log("entity doesn't exist");
       return;
     }
-    service = service ? this.config.service : this.config.entity.slice(7,11);
+    service = service ? this.config.service : this.config.entity.slice(7, 11);
     const json = JSON.parse(hass.states[entity].attributes.data);
     if (!json || !json.length) {
-      return
+      return;
     }
-    const view = this.config.image_style || 'poster';
-    const dateform = this.config.date || 'mmdd';
-    const icon = this.config.icon || json[0]['icon'];
-    const icon_hide = this.config.icon == 'none' ? 'display:none;' : '';
-    const icon_color = this.config.icon_color || 'white';
-    const flag_color = this.config.flag_color || 'var(--primary-color)';
+    const view = this.config.image_style || "poster";
+    const dateform = this.config.date || "mmdd";
+    const icon = this.config.icon || json[0]["icon"];
+    const icon_hide = this.config.icon == "none" ? "display:none;" : "";
+    const icon_color = this.config.icon_color || "white";
+    const flag_color = this.config.flag_color || "var(--primary-color)";
     const flag = this.config.flag == undefined ? true : this.config.flag;
-    const timeform = { "hour12": (this.config.clock != 24), "hour": "2-digit", "minute": "2-digit" };
-    const title_text = this.config.title_text || json[0]['title_default'];
-    const line1_text = this.config.line1_text || json[0]['line1_default'];
-    const line2_text = this.config.line2_text || json[0]['line2_default'];
-    const line3_text = this.config.line3_text || json[0]['line3_default'];
-    const line4_text = this.config.line4_text || json[0]['line4_default'];
-    const title_size = this.config.title_size || 'large';
-    const line1_size = this.config.line1_size || this.config.line_size || 'medium';
-    const line2_size = this.config.line2_size || this.config.line_size || 'small';
-    const line3_size = this.config.line3_size || this.config.line_size || 'small';
-    const line4_size = this.config.line4_size || this.config.line_size || 'small';
-    const tSize = (size) => size == 'large' ? '18' : size == 'medium' ? '14' : '12';
-    const size = [tSize(title_size), tSize(line1_size), tSize(line2_size), tSize(line3_size), tSize(line4_size)];
-    const defaultClr = (poster, fanart) => view == 'poster' ? poster : fanart;
-    const title_color = this.config.title_color || defaultClr('var(--primary-text-color)', '#fff');
-    const line1_color = this.config.line1_color || this.config.line_color || defaultClr('var(--primary-text-color)', '#fff');
-    const line2_color = this.config.line2_color || this.config.line_color || defaultClr('var(--primary-text-color)', '#fff');
-    const line3_color = this.config.line3_color || this.config.line_color || defaultClr('var(--primary-text-color)', '#fff');
-    const line4_color = this.config.line4_color || this.config.line_color || defaultClr('var(--primary-text-color)', '#fff');
-    const accent = this.config.accent_color || defaultClr('var(--primary-color)', '#000');
-    const border = this.config.border_color || defaultClr('#fff', '#000');
-    const shadows = (conf) => this.config.all_shadows == undefined ? conf == undefined ? true : conf : this.config.all_shadows;
-    const boxshdw = shadows(this.config.box_shadows) ? view == 'poster' ? '5px 5px 10px' : '3px 2px 25px' : '';
-    const svgshdw = shadows(this.config.box_shadows) ? 'url(#grad1)' : accent;
-    const txtshdw = shadows(this.config.text_shadows) ? '1px 1px 3px' : '';
+    const timeform = {
+      hour12: this.config.clock != 24,
+      hour: "2-digit",
+      minute: "2-digit"
+    };
+    const title_text = this.config.title_text || json[0]["title_default"];
+    const line1_text = this.config.line1_text || json[0]["line1_default"];
+    const line2_text = this.config.line2_text || json[0]["line2_default"];
+    const line3_text = this.config.line3_text || json[0]["line3_default"];
+    const line4_text = this.config.line4_text || json[0]["line4_default"];
+    const title_size = this.config.title_size || "large";
+    const line1_size =
+      this.config.line1_size || this.config.line_size || "medium";
+    const line2_size =
+      this.config.line2_size || this.config.line_size || "small";
+    const line3_size =
+      this.config.line3_size || this.config.line_size || "small";
+    const line4_size =
+      this.config.line4_size || this.config.line_size || "small";
+    const tSize = size =>
+      size == "large" ? "18" : size == "medium" ? "14" : "12";
+    const size = [
+      tSize(title_size),
+      tSize(line1_size),
+      tSize(line2_size),
+      tSize(line3_size),
+      tSize(line4_size)
+    ];
+    const defaultClr = (poster, fanart) => (view == "poster" ? poster : fanart);
+    const title_color =
+      this.config.title_color ||
+      defaultClr("var(--primary-text-color)", "#fff");
+    const line1_color =
+      this.config.line1_color ||
+      this.config.line_color ||
+      defaultClr("var(--primary-text-color)", "#fff");
+    const line2_color =
+      this.config.line2_color ||
+      this.config.line_color ||
+      defaultClr("var(--primary-text-color)", "#fff");
+    const line3_color =
+      this.config.line3_color ||
+      this.config.line_color ||
+      defaultClr("var(--primary-text-color)", "#fff");
+    const line4_color =
+      this.config.line4_color ||
+      this.config.line_color ||
+      defaultClr("var(--primary-text-color)", "#fff");
+    const accent =
+      this.config.accent_color || defaultClr("var(--primary-color)", "#000");
+    const border = this.config.border_color || defaultClr("#fff", "#000");
+    const shadows = conf =>
+      this.config.all_shadows == undefined
+        ? conf == undefined
+          ? true
+          : conf
+        : this.config.all_shadows;
+    const boxshdw = shadows(this.config.box_shadows)
+      ? view == "poster"
+        ? "5px 5px 10px"
+        : "3px 2px 25px"
+      : "";
+    const svgshdw = shadows(this.config.box_shadows) ? "url(#grad1)" : accent;
+    const txtshdw = shadows(this.config.text_shadows) ? "1px 1px 3px" : "";
     const max = Math.min(json.length - 1, this.config.max || 5);
     window.cardSize = max;
 
-    if (view == 'poster' && !this.querySelector('[id="umc_style"]')) {
+    if (view == "poster" && !this.querySelector('[id="umc_style"]')) {
       style.textContent = `
         .${service}_${view} {
           width:100%;
@@ -231,18 +269,21 @@ class UpcomingMediaCard extends HTMLElement {
         }
       `;
     }
-    this.content.innerHTML = '';
+    this.content.innerHTML = "";
 
     // Truncate text...
     function truncate(text, chars) {
       // When to truncate depending on size
-      chars = chars == 'large' ? 23 : chars == 'medium' ? 28 : 35;
+      chars = chars == "large" ? 23 : chars == "medium" ? 28 : 35;
       // Remove parentheses & contents: "Shameless (US)" becomes "Shameless".
-      text = text.replace(/ *\([^)]*\) */g, ' ');
+      text = text.replace(/ *\([^)]*\) */g, " ");
       // Truncate only at whole word w/ no punctuation or space before ellipsis.
       if (text.length > chars) {
         for (let i = chars; i > 0; i--) {
-          if (text.charAt(i).match(/( |:|-|;|"|'|,)/) && text.charAt(i - 1).match(/[a-zA-Z0-9_]/)) {
+          if (
+            text.charAt(i).match(/( |:|-|;|"|'|,)/) &&
+            text.charAt(i - 1).match(/[a-zA-Z0-9_]/)
+          ) {
             var truncated = `${text.substring(0, i)}...`;
             return truncated;
           }
@@ -255,19 +296,21 @@ class UpcomingMediaCard extends HTMLElement {
     function format_date(input_date) {
       // Match UTC ISO formatted date with time
       if (String(input_date).match(/[T]\d+[:]\d+[:]\d+[Z]/)) {
-        var fd_day = new Date(
-          input_date).toLocaleDateString([], { day: "2-digit" });
-        var fd_month = new Date(
-          input_date).toLocaleDateString([], { month: "2-digit" });
-      // Match date string. ie: 2018-10-31
+        var fd_day = new Date(input_date).toLocaleDateString([], {
+          day: "2-digit"
+        });
+        var fd_month = new Date(input_date).toLocaleDateString([], {
+          month: "2-digit"
+        });
+        // Match date string. ie: 2018-10-31
       } else if (String(input_date).match(/\d+[-]\d+[-]\d+/)) {
-        input_date = input_date.split('-');
+        input_date = input_date.split("-");
         fd_month = input_date[1];
         fd_day = input_date[2];
       } else {
-        return '';
+        return "";
       }
-      if (dateform == 'ddmm') {
+      if (dateform == "ddmm") {
         return `${fd_day}/${fd_month}`;
       } else {
         return `${fd_month}/${fd_day}`;
@@ -275,29 +318,36 @@ class UpcomingMediaCard extends HTMLElement {
     }
 
     for (let count = 1; count <= max; count++) {
-      const item = (key) => json[count][key];
-      if (!item('airdate')) continue;
-      let airdate = new Date(item('airdate'));
-      let dflag = item('flag') && flag ? '' : 'display:none;';
-      let image = view == 'poster' ? item('poster') : item('fanart') || item('poster');
-      let daysBetween = Math.round(Math.abs((new Date().getTime()-airdate.getTime())/(24*60*60*1000)));
-      let day = daysBetween <= 7 ?
-        airdate.toLocaleDateString([], { weekday: "long" }) :
-        airdate.toLocaleDateString([], { weekday: "short" });
+      const item = key => json[count][key];
+      if (!item("airdate")) continue;
+      let airdate = new Date(item("airdate"));
+      let dflag = item("flag") && flag ? "" : "display:none;";
+      let image =
+        view == "poster" ? item("poster") : item("fanart") || item("poster");
+      let daysBetween = Math.round(
+        Math.abs(
+          (new Date().getTime() - airdate.getTime()) / (24 * 60 * 60 * 1000)
+        )
+      );
+      let day =
+        daysBetween <= 7
+          ? airdate.toLocaleDateString([], { weekday: "long" })
+          : airdate.toLocaleDateString([], { weekday: "short" });
 
       // Format runtime as either '23 min' or '01:23' if over an hour
-      let hrs = String(Math.floor(item('runtime') / 60)).padStart(2, 0);
-      let min = String(Math.floor(item('runtime') % 60)).padStart(2, 0);
-      let runtime = item('runtime') > 0 ? hrs > 0 ? `${hrs}:${min}` : `${min} min` : '';
+      let hrs = String(Math.floor(item("runtime") / 60)).padStart(2, 0);
+      let min = String(Math.floor(item("runtime") % 60)).padStart(2, 0);
+      let runtime =
+        item("runtime") > 0 ? (hrs > 0 ? `${hrs}:${min}` : `${min} min`) : "";
 
       // Shifting images for fanart view since we use poster as fallback image.
-      let shiftimg = item('fanart') ?
-        'background-position:100% 0;' :
-        'background-size: 54% auto;background-position:100% 35%;';
+      let shiftimg = item("fanart")
+        ? "background-position:100% 0;"
+        : "background-size: 54% auto;background-position:100% 35%;";
 
       // First item in card needs no top margin.
-      if (count == '0') var top = '0px';
-      else view == 'poster' ? '20px' : '10px';
+      if (count == "0") var top = "0px";
+      else view == "poster" ? "20px" : "10px";
 
       let line = [title_text, line1_text, line2_text, line3_text, line4_text];
       let char = [title_size, line1_size, line2_size, line3_size, line4_size];
@@ -305,25 +355,25 @@ class UpcomingMediaCard extends HTMLElement {
       // Keyword map for replacement, return null if empty so we can hide empty sections
       let keywords = /\$title|\$episode|\$genres|\$number|\$rating|\$release|\$runtime|\$studio|\$day|\$date|\$time|\$aired/g;
       let keys = {
-        $title: item('title') || null,
-        $episode: item('episode') || null,
-        $genres: item('genres') || null,
-        $number: item('number') || null,
-        $rating: item('rating') || null,
-        $release: item('release') || null,
-        $studio: item('studio') || null,
+        $title: item("title") || null,
+        $episode: item("episode") || null,
+        $genres: item("genres") || null,
+        $number: item("number") || null,
+        $rating: item("rating") || null,
+        $release: item("release") || null,
+        $studio: item("studio") || null,
         $runtime: runtime || null,
         $day: day || null,
         $time: airdate.toLocaleTimeString([], timeform) || null,
-        $date: format_date(item('airdate')) || null,
-        $aired: format_date(item('aired')) || null
+        $date: format_date(item("airdate")) || null,
+        $aired: format_date(item("aired")) || null
       };
 
       // Replace keywords in lines
       for (let i = 0; i < line.length; i++) {
-        line[i] = line[i].replace(' - ', '-');
+        line[i] = line[i].replace(" - ", "-");
         // Split at '-' so we can ignore entire contents if keyword returns null
-        let text = line[i].replace(keywords, (val) => keys[val]).split('-');
+        let text = line[i].replace(keywords, val => keys[val]).split("-");
         let filtered = [];
         // Rebuild lines, ignoring null
         for (let t = 0; t < text.length; t++) {
@@ -331,20 +381,31 @@ class UpcomingMediaCard extends HTMLElement {
           else filtered.push(text[t]);
         }
         // Replacing twice to get keywords in component generated strings
-        text = filtered.join(' - ').replace(keywords, (val) => keys[val]);
+        text = filtered.join(" - ").replace(keywords, val => keys[val]);
 
         // Shifting header text around depending on view & size
         let svgshift, y;
-        if (i == 0) size[i].match(/18/) ? y = '-5' : size[i].match(/14/) ? y = '-2' : y = '0';
-        if (view == 'fanart') svgshift = i == 0 ? `x="0" dy="1em" ` : `x="0" dy="1.3em" `;
-        else svgshift = i == 0 ? `x="15" y="${y}" dy="1.3em" ` : `x="15" dy="1.3em" `;
+        if (i == 0)
+          size[i].match(/18/)
+            ? (y = "-5")
+            : size[i].match(/14/)
+            ? (y = "-2")
+            : (y = "0");
+        if (view == "fanart")
+          svgshift = i == 0 ? `x="0" dy="1em" ` : `x="0" dy="1.3em" `;
+        else
+          svgshift =
+            i == 0 ? `x="15" y="${y}" dy="1.3em" ` : `x="15" dy="1.3em" `;
 
         // Build lines HTML or empty line
-        line[i] = line[i].match('empty') ?
-          `<tspan class="${service}_line${i}_${view}" style="fill:transparent;text-shadow:0 0 transparent;" ${svgshift}>.</tspan>` :
-          `<tspan class="${service}_line${i}_${view}" ${svgshift}>${truncate(text,char[i])}</tspan>`;
+        line[i] = line[i].match("empty")
+          ? `<tspan class="${service}_line${i}_${view}" style="fill:transparent;text-shadow:0 0 transparent;" ${svgshift}>.</tspan>`
+          : `<tspan class="${service}_line${i}_${view}" ${svgshift}>${truncate(
+              text,
+              char[i]
+            )}</tspan>`;
       }
-      if (view == 'poster') {
+      if (view == "poster") {
         this.content.innerHTML += `
           <div id='main' class='${service}_${view}' style='margin-top:${top};'>
              <div class="${service}_container_${view}" style="background-image:url('${image}');">
@@ -374,7 +435,7 @@ class UpcomingMediaCard extends HTMLElement {
         `;
       } else {
         this.content.innerHTML += `
-          <div class="${service}_${view} style='${top}'"
+          <div class="${service}_${view}" style="${top}"
              style="${shiftimg}background-image:url('${image}')">
              <div class="${service}_fan_${view}">
                 <ha-icon icon="${icon}" style="${dflag}"></ha-icon>
@@ -384,7 +445,9 @@ class UpcomingMediaCard extends HTMLElement {
                    </svg>
                 </div>
                 <svg class="${service}_svg_${view}"viewBox="0 0 200 100">
-                   <text>${line[0]}${line[1]}${line[2]}${line[3]}${line[4]}</text>
+                   <text>${line[0]}${line[1]}${line[2]}${line[3]}${
+          line[4]
+        }</text>
                 </svg>
              </div>
           </div>
@@ -394,12 +457,13 @@ class UpcomingMediaCard extends HTMLElement {
     }
   }
   setConfig(config) {
-    if (!config.service && !config.entity) throw new Error('Define entity or service.');
+    if (!config.service && !config.entity)
+      throw new Error("Define entity or service.");
     this.config = config;
   }
   getCardSize() {
-    let view = this.config.image_style || 'poster';
-    return view == 'poster' ? window.cardSize * 5 : window.cardSize * 3;
+    let view = this.config.image_style || "poster";
+    return view == "poster" ? window.cardSize * 5 : window.cardSize * 3;
   }
 }
-customElements.define('upcoming-media-card', UpcomingMediaCard);
+customElements.define("upcoming-media-card", UpcomingMediaCard);
