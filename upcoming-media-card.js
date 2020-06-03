51187fd0a749d6de24a75513b61ac3a7e6d45133
Alexander Ryazanov <alryaz@xavux.com>
2020-06-04 02:37:01 +0300

    Fixed `undefined` on Unicode-named media with long names (#52)
    * Fixed `undefined` on Unicode-named media with long names
    
    I have movie titles translated in Russian, Korean, etc., and they show up as undefined, because that cycle never results in a return, and that letter matching never matches the letters. This should fix it.
    
    * Update upcoming-media-card.js
    
    Co-authored-by: Ryan Meek <25127328+maykar@users.noreply.github.com>

--- upcoming-media-card.js
+++ upcoming-media-card.js
@@ -271,19 +271,18 @@ class UpcomingMediaCard extends HTMLElement {
     // Truncate text...
     function truncate(text, chars) {
       // When to truncate depending on size
-      chars = chars == "large" ? 23 : chars == "medium" ? 28 : 35;
+      chars = chars == 'large' ? 23 : chars == 'medium' ? 28 : 35;
       // Remove parentheses & contents: "Shameless (US)" becomes "Shameless".
-      text = text.replace(/ *\([^)]*\) */g, " ");
+      text = text.replace(/ *\([^)]*\) */g, ' ');
       // Truncate only at whole word w/ no punctuation or space before ellipsis.
       if (text.length > chars) {
         for (let i = chars; i > 0; i--) {
-          if (
-            text.charAt(i).match(/( |:|-|;|"|'|,)/) &&
-            text.charAt(i - 1).match(/[a-zA-Z0-9_]/)
-          ) {
+          if (text.charAt(i).match(/( |\s|:|-|;|"|'|,)/) && text.charAt(i - 1).match(/[a-zA-Z0-9_]/)) {
             return `${text.substring(0, i)}...`;
           }
         }
+        // The cycle above had a really big single word, so we return it anyway
+        return `${text.substring(0, chars)}...`;
       } else {
         return text;
       }
